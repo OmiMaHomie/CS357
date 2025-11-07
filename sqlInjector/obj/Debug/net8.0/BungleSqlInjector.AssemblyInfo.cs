38eea6bfd0b2840e63cc5e2845d3a047334fa2ee
Om Khadka <om.khadka.101@gmail.com>
2025-11-07 00:36:49 -0500

    aded login lab documentation

--- sqlInjector/obj/Debug/net8.0/BungleSqlInjector.AssemblyInfo.cs
+++ sqlInjector/obj/Debug/net8.0/BungleSqlInjector.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BungleSqlInjector")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3dfebc8936cc8daaa9af9527da9d133ffe6ad3ba")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55b6b8f22d301669ad148818f7f39f63e846bbbb")]
 [assembly: System.Reflection.AssemblyProductAttribute("BungleSqlInjector")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BungleSqlInjector")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
